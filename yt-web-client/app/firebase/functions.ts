24d13c89c6c227bdcfd5b23de3e9fd4d09a4742d
Reddimus <Kevi.M.CECS@outlook.com>
2024-07-08 18:45:34 -0700

    refactor: Update firebase functions import and add getVideos function
    This commit updates the import statement for the firebase functions module in the `functions.ts` file. It also adds a new function `getVideos` that retrieves a list of videos from the Firestore collection. These changes improve the functionality of the server-side code for video management.

--- yt-web-client/app/firebase/functions.ts
+++ yt-web-client/app/firebase/functions.ts
@@ -1,8 +1,18 @@
-import {getFunctions, httpsCallable} from "firebase/functions";
-
-const functions = getFunctions();
+import {httpsCallable} from "firebase/functions";
+import {functions} from './firebase';
 
 const generateUploadUrl = httpsCallable(functions, 'generateUploadUrl');
+const getVideosFunction = httpsCallable(functions, 'getVideos');
+
+// TODO: Use GRPC to reduce number of interface Video initializations
+export interface Video {
+  id?: string,
+  uid?: string,
+  filename?: string,
+  status?: "processing" | "processed",
+  title?: string,
+  description?: string
+}
 
 export async function uploadVideo(file: File) {
   const response: any = await generateUploadUrl({
@@ -19,4 +29,9 @@ export async function uploadVideo(file: File) {
   });
 
   return uploadResult;
+}
+
+export async function getVideos() {
+  const response: any = await getVideosFunction();
+  return response.data as Video[];
 }
